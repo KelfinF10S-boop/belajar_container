dfd5c658c475fb8a0831bef598921c7d1e98654b
KelfinF10S-boop <anamalang762@gmail.com>
2024-11-12 11:32:33 +0700

    final version 1.0
    belajar container

---
+++ main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Root widget
+      home: Scaffold(
+        appBar: AppBar(
+          title: Container(
+            child: Text('Container Widget'),
+            color: Colors.deepPurple.shade200,
+            alignment: Alignment.center,
+            height: 100,
+          ),
+        ),
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 220,
+                      width: double.infinity,
+                      margin: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade200,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.deepPurple),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 8,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Title: Flutter Stuff',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          const SizedBox(height: 8),
+                          const Divider(color: Colors.black),
+                          const Text(
+                            'Description: This is a card-like layout using a Container widget. It has padding, margin, and a box shadow.',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                              height: 40,
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text('visit to read more')))
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
